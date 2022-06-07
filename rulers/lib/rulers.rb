6bf9eb17cf6c1d55d4ed100ece96a4883b6f587f
jim.h <hjijin.gz@gmail.com>
2022-06-08 00:20:34 +0800

    add controller.

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -1,15 +1,25 @@
 # frozen_string_literal: true
 
-require_relative "rulers/version"
+require "rulers/version"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
-      [
-        200, 
-        {'Content-Type' => 'text/html'},
-        ["Hello, world!"]
-      ]
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, {'Content-Type' => 'text/html'}, [text]]
     end
+  end
+
+  class Controller
+    def initialize(env) 
+      @env = env 
+    end
+
+    def env 
+      @env 
+    end 
   end
 end
