eb92a973e20ba074874da76627c706decbdf33ec
jim.h <hjijin.gz@gmail.com>
2022-06-09 00:08:20 +0800

    modify controller.

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
-require "rulers/version"
+require "rulers/version" 
 require "rulers/routing"
 
 module Rulers
-  class Application
-    def call(env)
-      klass, act = get_controller_and_action(env)
-      controller = klass.new(env)
+  class Application 
+    def call(env) 
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404,{'Content-Type' => 'text/html'}, []]
+      end
+      
+      klass, act = get_controller_and_action(env) 
+      controller = klass.new(env) 
       text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'}, [text]]
-    end
+      [200, {'Content-Type' => 'text/html'},[text]]
+    end 
   end
 
   class Controller
