143d54194bd2a2b5d12800aab04743ed638dc38a
Jim Huang <hjijin.gz@gmail.com>
2022-06-14 16:49:31 +0800

    using Rack’s Request and Response objects, giving you a lot of things like parameter parsing and session variables for free

--- rulers/lib/rulers/controller.rb
+++ rulers/lib/rulers/controller.rb
@@ -1,16 +1,40 @@
+require "erubis"
 require "rulers/file_model"
+require "rack/request"
 
 module Rulers
-  class Controller 
+  class Controller
     include Rulers::Model
-    
-    def initialize(env) 
-      @env = env 
+
+    def initialize(env)
+      @env = env
     end
 
-    def env 
-      @env 
-    end 
+    def env
+      @env
+    end
+
+    def request
+      @request ||= Rack::Request.new(@env)
+    end
+
+    def params
+      request.params
+    end
+
+    def response(text, status=200, headers = {})
+      raise "Already responded!" if @response
+      a = [text].flatten
+      @response = Rack::Response.new(a, status, headers)
+    end
+
+    def get_response
+      @response
+    end
+
+    def render_response(*args)
+      response(render(*args))
+    end
 
     def render(view_name, locals = {})
       filename = File.join "app", "views", controller_name, "#{view_name}.html.erb"
@@ -24,5 +48,5 @@ module Rulers
       klass = klass.to_s.gsub /Controller$/, ""
       Rulers.to_underscore klass
     end
-  end 
+  end
 end
