f6ae2de6bbaf2a5d2aaee5122609b82c43e0a639
Jim Huang <hjijin.gz@gmail.com>
2022-06-16 16:23:11 +0800

    built a full-on router.

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -8,22 +8,14 @@ require "rulers/controller"
 require "rulers/file_model"
 
 module Rulers
-  class Application 
-    def call(env) 
+  class Application
+    def call(env)
       if env['PATH_INFO'] == '/favicon.ico'
-        return [404,{'Content-Type' => 'text/html'}, []]
+        return [404, {'Content-Type' => 'text/html'}, []]
       end
-      
-      klass, act = get_controller_and_action(env) 
-      controller = klass.new(env) 
-      text = controller.send(act)
-      
-      if controller.get_response
-        st, hd, rs = controller.get_response.to_a
-        [st, hd, [rs].flatten]
-      else
-        [200, {'Content-Type' => 'text/html'}, [text]]
-      end
-    end 
+
+      rack_app = get_rack_app(env)
+      rack_app.call(env)
+    end
   end
 end
